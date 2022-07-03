96abe92364f18559112af8626efff56d8bc39c1d
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2022-07-03 10:55:42 +0530

    added enums for VerificationStatus and ReviewType

---
+++ src/main/java/com/gb/reviews/users/ReviewType.java
@@ -0,0 +1,6 @@
+package com.gb.reviews.users;
+
+public enum ReviewType {
+    CERTIFIED_BUYER,
+    ANONYMOUS
+}
---
+++ src/main/java/com/gb/reviews/users/VerificationStatus.java
@@ -0,0 +1,7 @@
+package com.gb.reviews.users;
+
+public enum VerificationStatus {
+    VERIFIED,
+    NON_VERFIFIED,
+    BLACK_LISTED,
+}
