fa766d9c0625e416e9e03168b952c676fd1dda90
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2022-07-04 16:29:01 +0530

    added user location

---
+++ src/main/java/com/gb/reviews/user/UserLocation.java
@@ -0,0 +1,15 @@
+package com.gb.reviews.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLocation {
+    private String address1;
+    private String address2;
+    private String address3;
+    private String city;
+    private String state;
+    private String pin;
+}
