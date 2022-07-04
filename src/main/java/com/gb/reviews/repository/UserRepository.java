1d210400cf93639fc335d5d597b73d92e0097ed7
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2022-07-04 09:08:52 +0530

    added repo class for persistence

---
+++ src/main/java/com/gb/reviews/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.gb.reviews.repository;
+
+import com.gb.reviews.user.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserRepository {
+    public static List<User> users = new ArrayList<>();
+    public static Map<String,User> usersMap = new HashMap<>();
+}
